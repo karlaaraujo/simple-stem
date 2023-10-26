66d6b11411deb4aeb3f264ec88be6b63c243c66b
Karla Araújo <karlachs.a@gmail.com>
2023-10-26 15:45:38 -0300

    feat: adicionar favicon.ico

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -8,6 +8,7 @@
 
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.bunny.net">
+        <link rel="icon" href="/favicon.ico" />
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Scripts -->
