4344f05b07d72dea36a7b3a42e51d004fa76e0b1
Karla Araújo <karlachs.a@gmail.com>
2023-10-30 21:07:39 -0300

    feat(home): adicionar funcionalidade à caixa de pesquisa da header

--- resources/js/Components/SimpleStemHeader.vue
+++ resources/js/Components/SimpleStemHeader.vue
@@ -1,41 +1,75 @@
 <template>
 
     <v-row style="width: 100%; height: 138px; background-color: #001c35; margin: 0">
-        <!-- <v-row style="width: 100%; height: 138px; background-color: #2D4356; margin: 0"></v-row> -->
         <div id="toolbar">
             <a class="header-logo" href="/">
-              <img width="192" height="49" viewBox="0 0 192 49" fill="none" src="@/Assets/logo_atualizada.svg" alt="logo" />
+                <img alt="logo" fill="none" height="49" src="@/Assets/logo_atualizada.svg" viewBox="0 0 192 49"
+                     width="192"/>
             </a>
-            <span>
-              <input type="text" class="search-input" placeholder="Busque artigos, tópicos..."/>
-              <svg-icon class="search-icon mt-n1" type="mdi" :path="path"></svg-icon>
-            </span>
+            <div class="search-container">
+                <input
+                    v-model="termoPesquisado"
+                    class="search-input"
+                    placeholder="Busque artigos, tópicos..."
+                    type="text"
+                    @keyup.enter="pesquisarTermo"
+                />
+                <a class="search-icon" :href="route('artigo.listar.termo', { termo: termoPesquisado })">
+                    <svg-icon
+                        :path="path"
+
+                        type="mdi"
+                    />
+                </a>
+
+            </div>
         </div>
     </v-row>
 
     <v-row class="simple-stem-row" style="width: 100%; height: 72px; background-color: #003249; margin: 0">
         <div class="simple-stem-nav">
-            <NavMenu siglaCategoria="S" :idCategoria="1" />
-            <NavMenu siglaCategoria="T" :idCategoria="2" />
-            <NavMenu siglaCategoria="E" :idCategoria="3" />
-            <NavMenu siglaCategoria="M" :idCategoria="4" />
+            <NavMenu :idCategoria="1" siglaCategoria="S"/>
+            <NavMenu :idCategoria="2" siglaCategoria="T"/>
+            <NavMenu :idCategoria="3" siglaCategoria="E"/>
+            <NavMenu :idCategoria="4" siglaCategoria="M"/>
         </div>
     </v-row>
 
     <span v-if="isMobile">
-        <ButtonsLoginCadastro />
+        <ButtonsLoginCadastro/>
     </span>
 
 </template>
 
 <script setup>
+import { ref } from 'vue';
 import NavMenu from './NavMenu.vue';
 import ButtonsLoginCadastro from './ButtonsLoginCadastro.vue';
+import SvgIcon from '@jamescoyle/vue-icon';
+import {mdiMagnify} from '@mdi/js';
+
+const path = mdiMagnify;
+const termoPesquisado = ref('');
+
 </script>
 
 <style scoped>
 @import url('https://fonts.googleapis.com/css2?family=Inter:wght@300;700&display=swap');
 
+.search-input {
+    padding-right: 2em;
+    border-radius: 15px;
+    border: 1px solid #000;
+    background: #FFF;
+    width: 100%;
+}
+
+.search-icon {
+    position: absolute;
+    right: 10px;
+    height: 20px;
+    width: 20px;
+}
 
 .simple-stem-row {
     display: flex;
@@ -54,26 +88,17 @@ div#toolbar {
     height: 138px;
 }
 
-.header-logo
-{
+.header-logo {
     margin-left: 7rem;
     margin-top: 30px;
 }
-span {
+
+.search-container {
     position: relative;
-    display: inline-block;
-}
-.search-input {
-    margin-right: 7rem;
-    padding: 0 8.5% 0 3%;
-    width: 370px;
-    height: 44px;
-    border-radius: 15px;
-    border: 1px solid #000000;
-    background-color: #fff;
-    color: #000000;
-    font-size: 12px;
-    font-weight: 100;
+    display: flex;
+    align-items: center;
+    margin-right: 5rem;
+    width: 280px;
 }
 
 .simple-stem-nav {
@@ -83,7 +108,8 @@ span {
     justify-content: space-evenly;
     cursor: pointer;
 }
-.stem-nav-hover{
+
+.stem-nav-hover {
     font-size: 36px;
     color: #FFF;
     font-weight: 300;
@@ -92,7 +118,8 @@ span {
 .stem-nav-hover:hover {
     font-weight: 700;
 }
-#nav-menu, .v-overlay-container{
+
+#nav-menu, .v-overlay-container {
     margin-top: .7%;
     background-color: #737373;
     opacity: 95%;
@@ -115,13 +142,22 @@ span {
         margin-left: 5rem;
     }
 
-    .search-input {
+    .search-container {
+        position: relative;
+        display: flex;
+        align-items: center;
         margin-right: 5rem;
         width: 280px;
-    } 
+    }
 }
 
-@media screen and (max-width: 767px){
+@media screen and (max-width: 767px) {
+
+    .search-container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+    }
 
     div#toolbar {
         display: flex;
@@ -137,30 +173,6 @@ span {
         margin-top: 30px;
     }
 
-    span {
-        display: flex;
-        flex-direction: column;
-    }
-
-    .search-icon {
-        margin-top: 4%;
-    }
-    input.search-input {
-        width: 280px;
-        height: 44px;
-        align-items: center;
-        justify-content: center;
-        margin-right: 0;
-        margin-bottom: 55px;
-        padding: 0 10% 0 3%;
-    }
-
-    svg.search-icon {
-        width: 19px;
-        height: 19px;
-        right: 10px;
-        top: 24px;
-    }
     .simple-stem-nav {
         width: auto;
         min-width: 320px;
